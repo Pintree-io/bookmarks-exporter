b47bb561479f44366e5d38218a19b7950a999f78
Evan Long <evanlong0803@gmail.com>
2024-07-02 00:01:32 +0800

    refactor: Move Chrome bookmarks retrieval function to separate file

---
+++ src/utils/bookmark/chrome.ts
@@ -0,0 +1,17 @@
+export const getChromeBookmarks = async () => {
+  try {
+    const bookmarks = await new Promise((resolve, reject) => {
+      chrome.bookmarks.getTree((bookmarks) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(bookmarks);
+        }
+      });
+    });
+    return bookmarks;
+  } catch (error) {
+    console.error('Error fetching bookmarks:', error);
+    throw error;
+  }
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,22 +3,3 @@ export const getCopyYear = () => {
   const year = currentYear === 2024 ? "2024" : `2024 - ${currentYear}`
   return year
 }
-
-
-export const getChromeBookmarks = async () => {
-  try {
-    const bookmarks = await new Promise((resolve, reject) => {
-      chrome.bookmarks.getTree((bookmarks) => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-        } else {
-          resolve(bookmarks);
-        }
-      });
-    });
-    return bookmarks;
-  } catch (error) {
-    console.error('Error fetching bookmarks:', error);
-    throw error;
-  }
-};
