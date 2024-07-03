1ed30c65ed38212610db17926c3f99b1753ef834
Evan Long <evanlong0803@gmail.com>
2024-07-03 19:44:00 +0800

    refactor: Add MainFooter component

---
+++ src/components/main-footer.tsx
@@ -0,0 +1,26 @@
+import { Icon } from "@/components/icons"
+import { getCopyYear } from "@/utils"
+
+export function MainFooter() {
+  return (
+    <footer className="text-zinc-600 flex items-center justify-center">
+      <span className="mr-3">
+        &copy;&nbsp;{getCopyYear()}&nbsp;
+        <a href="https://pintree.io" target="_blank" rel="noreferrer">
+          Pintree.io
+        </a>
+      </span>
+      <a
+        href="htttps://github.com/pintreeio"
+        className="flex items-center"
+        target="_blank"
+        rel="noreferrer">
+        <Icon
+          icon="grommet-icons:github"
+          className="w-3.5 h-3.5 text-zinc-600 mr-1.5 mb-0.5"
+        />
+        GitHub
+      </a>
+    </footer>
+  )
+}
